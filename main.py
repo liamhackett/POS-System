bd3bcd6e5f1d4d87b03934013fe073b13c58cf7d
Liam Hackett <ljh1oo@yahoo.com>
2022-11-28 16:40:02 -0500

    Added admin mode

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 from datetime import date
+import time
 
 def login(cur):
     username = ""
@@ -79,6 +80,7 @@ def display_inventory(cur):
     cur.execute("SELECT * FROM inventory")
     inventory = cur.fetchall()
     for item in inventory:
+        time.sleep(.25)
         print(f'{item[0]}: {item[1]} | ${item[2]}.00')
 
 
@@ -127,19 +129,71 @@ def purchase(user_id, cart, conn, cur):
         cur.execute(f"INSERT INTO purchases (user_id, inventory_id, cost, quantity, dop) VALUES ({user_id}, {item[0]}, {item[1]}, {item[2]}, '{today}')")
 
     conn.commit()
-    print("Purchase complete")
-
+    print("Purchase complete\n")
+    # ADD LINE TO UPDATE COUNT IN INVENTORY
 
 def view_purchases(user_id, cur):
-    cur.execute(f"SELECT * FROM purchases WHERE user_id={user_id} ORDER BY dop")
+    cur.execute(f"SELECT purchase_id, cost, quantity, dop FROM users INNER JOIN purchases ON users.user_id == purchases.user_id WHERE purchases.user_id == {user_id} ORDER BY dop")
     purchases = cur.fetchall()
-    print("Purchases:")
     for purchase in purchases:
         cur.execute(f"SELECT name FROM inventory WHERE inventory_id = {purchase[2]}")
         name_data = cur.fetchall()
         name = name_data[0][0]
-        print(f"{purchase[2]}: {name} | ${purchase[3]}.00 | {purchase[4]} | {purchase[5]}")
+        print(f"{purchase[0]}: {name} | ${purchase[1]}.00 | {purchase[2]} | {purchase[3]}")
 
+
+def view_inventory(cur):
+    cur.execute(f'SELECT * FROM inventory')
+    inventory = cur.fetchall()
+    for item in inventory:
+        print(f'{item[0]}: {item[1]} | ${item[2]}.00 | {item[3]}')
+
+def edit_inventory(cur, conn):
+    print("Enter an Inventory Id")
+    id = input("=> ")
+    print("Choose a section to edit\n1: Name\n2: Price\n3: Quantity")
+    col = input("=> ")
+    print("Enter a new value")
+    val = input("=> ")
+    try: 
+        if col == "1":
+            cur.execute(f"UPDATE inventory SET name = '{val}' WHERE inventory_id = {id}")
+        elif col == "2":
+            cur.execute(f"UPDATE inventory SET price = {val} WHERE inventory_id = {id}")
+        elif col == "3":
+            cur.execute(f"UPDATE inventory SET quantity = {val} WHERE inventory_id = {id}")
+    except:
+        print("Invalid input")
+        user_input = input("Would you like to try again? (y/n): ")
+        while user_input != "y" and user_input != "n":
+            print("Invalid input (y/n)")
+            user_input = input("Would you like to try again? (y/n): ")
+
+        if user_input == "y":
+            edit_inventory(conn, cur)
+    conn.commit()
+
+def view_all_purchases(cur):
+    cur.execute("SELECT purchase_id, inventory_id, first_name, last_name, cost, quantity dop FROM purchases INNER JOIN users ON purchases.user_id == users.user_id")
+    purchases = cur.fetchall()
+    print(purchases)
+    for purchase in purchases:
+        cur.execute(f"SELECT name FROM inventory WHERE inventory_id = {purchase[1]}")
+        name_data = cur.fetchall()
+        name = name_data[0][0]
+        print(f"{purchase[0]}: {purchase[2]} {purchase[3]} | {name} | {purchase[4]} | {purchase[5]}")
+
+def view_users(cur):
+    cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    print(users)
+    for user in users:
+        print(f"{user[0]}: {user[1]} {user[2]}\n username: {user[3]}\n email: {user[5]}\n address: {user[6]}\n phone number: {user[7]}")
+
+def delete_user(cur, conn):
+    print("Enter a user id to delete")   
+    id = input("=> ")
+    cur.execute(f"DELETE FROM users WHERE user_id == {id}")
 
 def main():
     conn = sqlite3.connect("users.db")
@@ -158,9 +212,55 @@ def main():
     if logged_in:
         print("Successful Login")
         
-        cur.execute(f"SELECT 'First Name', 'Last Name', user_id FROM users WHERE username = '{username}'")
+        cur.execute(f"SELECT first_name, last_name, user_id, admin FROM users WHERE username = '{username}'")
         name = cur.fetchall()
         user_id = name[0][2]
+        admin = name[0][3]
+        
+        if admin == 1:
+            admin_mode = input("Would you like to enter Admin mode? (y/n): ")
+            while admin_mode != "y" and admin_mode != "n":
+                print("Invalid Input")
+                admin_mode = input("=> ")
+            while admin_mode == "y":
+                print("Admin mode")
+                print("Choose an Option:\na) View Inventory\nb) View Purchases\nc) View Users\nd) Enter Customer Mode\ne) Log-out")
+                user_input =  input("=> ")
+                while user_input != "a" and user_input !="b" and user_input != "c" and user_input != "d" and user_input != "e":
+                    print("Invalid Input")
+                    user_input =  input("=> ")
+
+                if user_input == "a":
+                    view_inventory(cur)
+                    print("Choose an option:\n a) Go Back\n b) Edit")
+                    temp_input = input("=> ")
+                    while temp_input != "b" and temp_input !="a":
+                        temp_input = input("Invalid Input => ")
+                    if temp_input == "b":
+                        edit_inventory(cur, conn)
+                
+                elif user_input == "b":
+                    view_all_purchases(cur)
+                    print("Choose an option:\n a) Go Back")
+                    temp_input = input("=> ")
+                    while temp_input !="a":
+                        temp_input = input("Invalid Input => ")
+                
+                elif user_input == "c":
+                    view_users(cur)
+                    print("Choose an option:\n a) Go Back\n b) Delete User")
+                    temp_input = input("=> ")
+                    while temp_input != "b" and temp_input !="a":
+                        temp_input = input("Invalid Input => ")
+                    if temp_input == "b":
+                        delete_user(cur, conn)
+
+                elif user_input == "d":
+                    admin_mode = ""
+
+                elif user_input == "e":
+                    admin_mode = ""
+                    quit = True
 
         while not quit:
 
